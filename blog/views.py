d9194f00fccd044a2eaefa94cf1c0293247cd589
HelloWorldIsNotDefined <158848243+HelloWorldIsNotDefined@users.noreply.github.com>
2024-10-15 00:23:19 +0330

    Update views.py
    I removed confirm_delete and improved delete function for less code to confirm deletion.

--- blog/views.py
+++ blog/views.py
@@ -78,40 +78,17 @@ def update(request, blog_id):
 
 @login_required(login_url='accounts:login')
 def delete(request, blog_id):
-    """ Redirect the user to another page to confirm the deletion """
+    """ Delete the blog """
     blog = Blog.objects.get(pk=blog_id)
     if request.user == blog.author:
-        title = blog.title
-        request.session['confirm'] = {
-            'pk':blog_id,
-            'title':title,
-        }
-        return redirect('blog:confirm')
+        if request.method == "POST":
+            blog.delete()
+            return redirect('blog:list')
+        
+        else:
+            return render(request, "blog/delete.html", {'blog':blog})
     
     else:
         messages.error(request, "You don't have permission to delete this blog.", 'danger')
         return redirect('blog:index')
 
-@login_required(login_url='accounts:login')
-def confirm_delete(request):
-    """ Delete a blog after user confirmation """
-     # If the user comes from another page, it raises a key validation error, so we don't allow the user to access this page.
-    try:
-        data = request.session['confirm']
-    except:
-        messages.error(request, "You don't have access to this page. ", 'danger')
-        return redirect('blog:index')
-    
-    title = data['title']
-    if request.method == 'POST':
-        if request.POST.get('confirm') == 'Yes':
-            blog = Blog.objects.get(pk=data['pk'])
-            blog.delete()
-            messages.success(request, 'Blog deleted successfully.')
-            del request.session['confirm']
-            return redirect('blog:list')
-        
-        elif request.POST.get('confirm') == 'Cancel':
-            return redirect('blog:list')
-
-    return render(request, 'blog/confirm.html', {'title':title})
