4e56f38035eb548fcd7562c553e2479bffbb2579
HelloWorldIsNotDefined <158848243+HelloWorldIsNotDefined@users.noreply.github.com>
2025-01-14 00:32:25 +0330

    Update views.py
    Improved create and update views

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,15 @@ def index(request):
     blogs = Blog.objects.all().order_by('-published_date')
     return render(request, 'blog/index.html', {'blogs':blogs})
 
+
+
 @login_required(login_url='accounts:login')
 def detail(request, blog_id):
     """ Get a specific blog """
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog':blog})
+
+
 
 @login_required(login_url='accounts:login')
 def list(request):
@@ -22,36 +26,33 @@ def list(request):
     blogs = Blog.objects.filter(author=request.user).order_by('-published_date')
     return render(request, 'blog/list.html', {'blogs':blogs})
 
+
+
 @login_required(login_url='accounts:login')
 def create(request):
     """ Create a new blog """
+    form = BlogCreationForm
+
     if request.method == 'POST':
         form = BlogCreationForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            blog = Blog.objects.create(
-                author=request.user,
-                title=data['title'],
-                text=data['text'],
-            )
-            if blog:
-                blog.save()
-                messages.success(request, "Blog created successfully.")
-                return redirect('blog:list')
-            
-            else:
-                messages.error(request, "There is an error.", 'danger')
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+
+            messages.success(request, "Blog created successfully.")
+            return redirect('blog:list')
                 
-        return render(request, 'blog/create.html', {'form':form})
-    
-    else:
-        form = BlogCreationForm
-        return render(request, 'blog/create.html', {'form':form})
+    return render(request, 'blog/create.html', {'form':form})
+
+
 
 @login_required(login_url='accounts:login')
 def update(request, blog_id):
     """ Modify the blog """
     blog = Blog.objects.get(pk=blog_id)
+    form = BlogUpdateForm(instance=blog)
+
     if request.user != blog.author:
         messages.error(request, "You don't have permission to delete this blog.", 'danger')
         return redirect('blog:index')
@@ -59,22 +60,15 @@ def update(request, blog_id):
     if request.method == 'POST':
         form = BlogUpdateForm(request.POST, instance=blog)
         if form.is_valid():
-            data = form.cleaned_data
+            blog = form.save(commit=False)
             if blog.author == request.user:
-                 blog.title = data['title']
-                 blog.text = data['text']
                  blog.save()
                  messages.success(request, f'{blog.title} updated successfully. ')
                  return redirect('blog:list')
              
-            else:
-                messages.error(request, 'You are not allowed do edit this blog. ', 'danger')
-                
-        return render(request, 'blog/update.html', {'form':form})
-    
-    else:
-        form = BlogUpdateForm(instance=blog)
-        return render(request, 'blog/update.html', {'form':form})
+    return render(request, 'blog/update.html', {'form':form})
+
+
 
 @login_required(login_url='accounts:login')
 def delete(request, blog_id):
